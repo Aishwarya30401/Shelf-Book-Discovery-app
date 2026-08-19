093c547069d61ff477b689af1c75d8aff9b6861d
Aishwarya V <aishu30401@gmail.com>
2026-08-19 18:08:04 +0530

    Fix Netlify Nuxt deployment configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,22 +3,22 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   compatibilityDate: '2026-08-18',
-
   css: ['~/assets/css/main.css'],
 
   runtimeConfig: {
-    googleBooksApiKey: '',
+    googleBooksApiKey: process.env.NUXT_GOOGLE_BOOKS_API_KEY,
   },
 
-  nitro: {
-    preset: 'netlify',
-  },
+  // nitro: {
+  //   preset: 'netlify',
+  // },
 
+  // modules: ['@nuxtjs/storybook'],
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
 
-  // modules: ['@nuxtjs/storybook'],
+  modules: ['@netlify/nuxt'],
 })
